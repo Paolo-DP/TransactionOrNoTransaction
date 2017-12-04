e75e815b4ee8c6e2b9c1c23810d98ed51337a2c1
Paolo <madpaolo079@gmail.com>
2017-12-05 00:48:04 +0800

    initial commit

---
+++ app/src/main/java/com/example/paolo/transactionornotransaction/Banker.java
@@ -0,0 +1,13 @@
+package com.example.paolo.transactionornotransaction;
+
+/**
+ * Created by Paolo on 05/12/2017.
+ */
+
+public class Banker {
+
+    public int getOffer(){
+        return 0;
+    }
+
+}
