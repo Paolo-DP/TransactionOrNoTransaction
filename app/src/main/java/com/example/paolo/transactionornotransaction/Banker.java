976b467a47641b6f84c6dee0831b1bda6f853030
Paolo <madpaolo079@gmail.com>
2017-12-06 18:38:14 +0800

    initial methods setup

--- app/src/main/java/com/example/paolo/transactionornotransaction/Banker.java
+++ app/src/main/java/com/example/paolo/transactionornotransaction/Banker.java
@@ -5,9 +5,19 @@ package com.example.paolo.transactionornotransaction;
  */
 
 public class Banker {
-
+    /*
+    This method will return the value of the Banker's offer
+    based on the previous picks as well as the
+    remaining cases
+     */
     public int getOffer(){
         return 0;
     }
 
+    /*
+    The main game activity will pass the list of
+     */
+    public void giveCasePicks(int[] casepicks){
+
+    }
 }
--- app/src/main/java/com/example/paolo/transactionornotransaction/MainGameActivity.java
+++ app/src/main/java/com/example/paolo/transactionornotransaction/MainGameActivity.java
@@ -1,13 +1,48 @@
 package com.example.paolo.transactionornotransaction;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainGameActivity extends AppCompatActivity {
-
+    /*
+    This is the main Game Activity
+    This Screen would show Buttons as the
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_game);
+
     }
+
+    /*
+    This would be the onClick method for all the suitcases
+    in the GUI.
+     */
+    public void caseClick(View view){
+
+    }
+
+    public boolean[] getAlreadyPickedCases(){
+        return null;
+    }
+
+    /*
+    returns an array of the money values the contestant
+    picked during a round
+     */
+    public int[] getRoundMoneyPicks(){
+        return null;
+    }
+
+    /*
+    returns an array of all the Money vales still
+    available on the board
+     */
+    public int[] getRemainingCaseValues(){
+        return null;
+    }
+
 }
