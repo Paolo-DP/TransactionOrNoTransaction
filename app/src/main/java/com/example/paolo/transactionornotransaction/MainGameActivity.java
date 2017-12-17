dc5ba37cfaa151d03ff635840c9e26f8107cc024
Paolo <madpaolo079@gmail.com>
2017-12-17 18:53:56 +0800

    added randomizer

--- app/src/main/java/com/example/paolo/transactionornotransaction/MainGameActivity.java
+++ app/src/main/java/com/example/paolo/transactionornotransaction/MainGameActivity.java
@@ -5,7 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.Random;
+
 public class MainGameActivity extends AppCompatActivity {
+    int maxmoney=1000000;
+    int numcases=10;
+    int[] money_values;
+    int[] money_cases;
+    boolean[] case_already_picked;
+
+    int maxcasepicks=numcases/4;
+    int picks=0;
+    int round=0;
+
     /*
     This is the main Game Activity
     This Screen would show Buttons as the
@@ -23,6 +35,12 @@ public class MainGameActivity extends AppCompatActivity {
      */
     public void caseClick(View view){
 
+        if(picks>(maxcasepicks-round))
+            picks++;
+        else{
+            picks=0;
+            round++;
+        }
     }
 
     public boolean[] getAlreadyPickedCases(){
@@ -43,6 +61,32 @@ public class MainGameActivity extends AppCompatActivity {
      */
     public int[] getRemainingCaseValues(){
         return null;
+    }
+
+    private void initGameCases(){
+        double basemoney = maxmoney/(Math.pow(2,numcases-1));
+        money_cases = new int[numcases];
+        money_values = new int[numcases];
+        case_already_picked = new boolean[numcases];
+        //System.out.println("Money Values:");
+        for(int i=0; i<numcases; i++){
+            money_values[i] = (int)(basemoney*Math.pow(2,i));
+            money_cases[i]=0;
+            case_already_picked[i]=true;
+            //System.out.println(money_values[i]);
+        }
+        int random;
+        for(int i=numcases-1; i>=0; i--){
+            do{
+                random = (int) (Math.random() * (numcases));
+                //System.out.println("Random: "+random);
+                if(case_already_picked[random]){
+                    money_cases[i] = money_values[random];
+                    case_already_picked[random]=false;
+                    //System.out.println("Case "+ (i)+": "+money_cases[i]);
+                }
+            }while(case_already_picked[random]);
+        }
     }
 
 }
